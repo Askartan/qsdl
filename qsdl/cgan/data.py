a29512106a4a88e1946cbe79b35df5fa6c08ad7b
Askartan <tategami@tuta.io>
2026-08-13 16:25:00 +0200

    training loop rekonstrukcji

---
+++ qsdl/cgan/data.py
@@ -0,0 +1,50 @@
+import h5py as hdf
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+
+
+class ReconstructionDataset(Dataset):
+    def __init__(self, h5_path, indices=None):
+        with hdf.File(f"{h5_path}", "r") as f:
+            self.wigners = np.asarray(f["wigner"][:])           #(N_samples, 128, 128)
+            self.rhos_clean = np.asarray(f["rhos_clean"][:])    #(N_samples, 128, 128)
+
+            if indices is not None:
+                self.wigners = self.wigners[indices]
+                self.rhos_clean = self.rhos_clean[indices]
+
+    def __len__(self):
+        return len(self.wigners)
+
+    def __getitem__(self, i: int):
+        w = torch.from_numpy(self.wigners[i]).float().unsqueeze(0) # (1,128,128) do pytorcha
+        rho = torch.from_numpy(self.rhos_clean[i])                 # (128, 128) complex
+        return w, rho
+
+def stratified_split(N, rng, train, test, val):
+    suma = train + test + val
+    if not (abs(suma - 1) < 1e-9 or abs(suma - 100)) < 1e-9:
+        raise ValueError(f"Parametry train: {train} test: {test} val: {val} musza sie rownac 1 lub 100%")
+    if abs(suma - 100) < 1e-9:
+        train, test, val = train / 100, test / 100, val / 100
+
+    train_idx, test_idx, val_idx  = [], [], []
+    N_class = N // 7
+
+    len_train = int(np.floor(N_class * train))
+    len_test = int(np.floor(N_class * test))
+    len_val = int(np.floor(N_class * val))
+
+    ixs = np.arange(0, N)
+
+    for i in range(7):
+        class_ixs = ixs[i * N_class : (i+1) * N_class]
+        shuffled = rng.permutation(class_ixs)
+        groups = np.split(shuffled, np.cumsum([len_train, len_test, len_val])[:-1])
+
+        train_idx.extend(list(groups[0]))
+        test_idx.extend(list(groups[1]))
+        val_idx.extend(list(groups[2]))
+
+    return train_idx, test_idx, val_idx
--- qsdl/cgan/training.py
+++
@@ -1,9 +0,0 @@
-import torch
-import torch.nn as nn
-import torch.optim as optim
-import torchvision
-from torch.utils.data import DataLoader
-from torchvision import datasets, transforms
-
-
-def GAN_training():
